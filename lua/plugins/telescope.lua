8df8368a05953551a608b7ad25cd7079ad22b753
Wiliam_Hung <william_hung@trendmicro.com>
2025-09-24 22:47:14 +0700

    chore: fzf case with non-casesenstive

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
         "nvim-telescope/telescope.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         },
         keys = {
             {
@@ -72,7 +72,7 @@ return {
                     fuzzy = true,
                     override_generic_sorter = true,
                     override_file_sorter = true,
-                    case_mode = "smart_case",
+                    case_mode = "ignore_case",
                 },
             },
         },
