01ca59ee368f21fa2820c97eeeaf04d299235900
Wiliam_Hung <william_hung@trendmicro.com>
2025-09-27 22:04:48 +0700

    feat: add plugins for dev util
    - rename theme to color-scheme.lua
    - game-typr for type practice
    - gitsigns for seeing git hunk with keymap <leader>gp
    - snacks.lua to explicitly write out enabled plugins

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,9 +1,21 @@
 return {
     "folke/snacks.nvim",
     opts = {
-        scroll = {
-            enabled = false,
-        },
+        -- enable plugins
+        indent = { enabled = true },
+        quickfile = { enabled = true },
+        rename = { enabled = true },
+        bigfile = { enabled = true },
+        explorer = { enabled = true },
+        picker = { enabled = true },
+        words = { enabled = true },
+        statuscolumn = { enabled = true },
+
+        -- disable plugins
+        scroll = { enabled = false },
+        git = { enabled = false },
+        input = { enabled = false },
+        notifier = { enabled = false },
+        scope = { enabled = false },
     },
 }
-
